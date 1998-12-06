eeee484fa2fbea4a58696348236b83cd66d20f5e
welch <welch>
1998-12-06 23:31:59 +0000

    Added configuration file that is separate from the startup script

--- bin/httpd.tcl
+++ bin/httpd.tcl
@@ -1,29 +1,28 @@
 #!/bin/sh
 #
+# Tcl HTTPD
+#
 # This is the main script for an HTTP server. 
 # To test out of the box, do
 # tclsh httpd -debug
 # or
 # wish httpd -debug
 #
-# Ordinarily you'll want to
-# edit this script to set various parameters, plus you'll
-# want to set the path to wish or tclsh
-# (Tcl 7.5 or higher is required,
-# this works with the latest version of Tcl 8.0, too)
+# Ordinarily you'll not want to edit this file.
+# For a quick spin, just pass the appropriate settings via the command line.
+# For fully custom operation, copy tclhttpd.rc to your own configuration file,
+# modify it, and specify it as the value of the -config command line option.
 #
-# For a quick spin, see the settings under QUICK START.
-# For fully custom operation, see the settings under CONFIGURATION.
+# Tcl 7.5 or higher is required, this works with the latest version of Tcl 8.0, too
+#
 #
 # Copyright (c) 1997 Sun Microsystems, Inc.
-# Copyright (c) 1998 Scriptics Corporation
 #
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
-# SCCS: @(#) httpd 1.17 97/12/15 09:21:53
 #
 # \
-exec wish8.0 "$0" ${1+"$@"}
+exec tclsh8.0 "$0" ${1+"$@"}
 
 # Bootstrap the location of the script library.  This requires that
 # the main httpd script be in a peer directory (e.g., ./bin and ./lib)
@@ -33,29 +32,73 @@ exec wish8.0 "$0" ${1+"$@"}
 set home [string trimright [file dirname [info script]] ./]
 set home [file join [pwd] $home]
 set Httpd(library) [file join [file dirname $home] lib]
-lappend auto_path $Httpd(library)
+regsub -all { } $tcl_platform(os) {} tmp
+lappend auto_path $Httpd(library) \
+    [file join $Httpd(library) Binaries $tmp] \
+    [file join $Httpd(library) Binaries $tmp $tcl_platform(osVersion)]
+unset tmp
 
-#QUICK CONFIG
-# The server comes with a URL tree under htdocs
-set docRoot	[file join [file dirname $home] htdocs]
-set port 	8015			;# 80 is standard, of course
-set host 	[info hostname]		;# Server name, really
-#set ipaddr	1.2.3.4			;# To bind to a specific interface
-set webmaster	webmaster		;# An email address for errors
-set uid		60001			;# Nobody, for setuid
-set gid		50			;# web group, for setgid
-set logDir	/tmp			;# Location of log files
-#end QUICK
+#
+# Define the command line option processing procedure
+#
+package require opt
+::tcl::OptProc _ProcessOptions [list \
+        [list -docRoot      -any    [file join $home sitedocs]      {Root directory for documents}] \
+        [list -port         -int    8015                              {Port number server is to listen on}] \
+        [list -host         -any    [info hostname]                 {Server name, should be fully qualified}] \
+        [list -ipaddr       -any    {}                              {Interface server should bind to}] \
+        [list -webmaster    -any    webmaster@[info hostname]       {E-mail address for errors}] \
+        [list -uid          -int    50                              {User Id that server ans scripts are to run under}] \
+        [list -gid          -int    100                             {Group Id for caching templates}] \
+        [list -config       -any    [file join $home tclhttpd.rc]   {Configuration File}] \
+        [list -library      -any    {}                              {Directory list where custom packages and auto loads are}] \
+	[list -debug	    -boolean false			    {If true, start interactive command loop}] \
+    ] {
 
-# Required modules
-package require httpd			;# Protocol stack
-package require html			;# Simple html generation
-package require url			;# URL dispatching
-package require auth			;# Basic authentication
-package require counter			;# Statistics
-package require mtype			;# Mime content types
-package require stdin			;# command reader
-package require utils			;# junk
+    # Map the local variables defined by OptProc onto the globals used by the server
+
+    upvar #0 \
+	docRoot     gdocRoot \
+	port        gport \
+	host        ghost \
+	ipaddr      gipaddr \
+	webmaster   gwebmaster \
+	uid         guid \
+	gid         ggid \
+	debug       gdebug \
+	config_file config_file \
+	auto_path   auto_path
+	
+    if {[string length $library]} then {
+	set auto_path [concat $auto_path $library]
+    }
+    foreach var {docRoot port host webmaster uid gid debug ipaddr} {
+	set g$var [set $var]
+    }
+    set config_file $config
+}
+
+eval _ProcessOptions $argv
+
+# Core modules
+package require httpd           ;# Protocol stack
+package require html            ;# Simple html generation
+package require url		;# URL dispatching
+package require counter         ;# Statistics
+package require mtype           ;# Mime content types
+package require utils           ;# junk
+
+#  Not strictly required, but nearly always used
+package require auth            ;# Basic authentication
+package require log             ;# Standard loggin
+
+# When debugging, a command reader is helpful
+if {$debug} {
+    if {[catch {package require stdin}]} {
+	puts "No command loop available"
+	set debug 0
+    }
+}
 
 # This automatically uses Tk for image maps and
 # a simple control panel.  If you have a Tcl-only shell,
@@ -75,128 +118,55 @@ if [info exists tk_version] {
     package require ismaptcl
 }
 
-# These packages are required for "normal" web servers
+# This initializes some state, but doesn't start the server yet.
+# Do this before loading the configuraiton file.
 
-package require doc		;# Basic file URLS
-package require include		;# Server side includes
-#package require safetcl	;# Safe tcl in external process (broken)
-package require cgi		;# Standard CGI
-package require log		;# Logging
-package require dirlist		;# Directory listings
-
-# These packages are for special things built right into the server
-
-package require direct		;# Application Direct URLs
-package require status		;# Built in status counters
-package require admin		;# Url-based administration
-package require mail		;# Crude email support
-package require session		;# Session state module (better Safe-Tcl)
-
-# Debugging support
-
-package require debug		;# /debug URL tree
-package require opentrace	;# wrapper for open command
-catch {source $Httpd(library)/prodebug.tcl}     ;# Tcl-Pro nub
-
-# These packages are for the SNMP demo application
-
-if [catch {
-package require snmp		;# SNMP form creation
-package require Tnm		;# Low level network stuff
-Snmp_SessionPage /snmp/host.snmp?session=new	;# Default start page
-}] {
-    catch {puts stderr "No SNMP support"}
-}
-
-# This call defines a few default settings
 Httpd_Init
 
-# CONFIGURATION
-# You may want adjust these parameters.
-# For information about these calls, see htdocs/reference.html
-
-Doc_Root                $docRoot
-Doc_IndexFile           index.{tml,html,shtml,thtml,htm,subst}
-Doc_PublicHtml          public_html
-Cgi_Directory           /cgi-bin
-Status_Url              /status
-Debug_Url               /debug
-Mail_Url                /mail
-Admin_Url               /admin
-Doc_TemplateInterp      {}
-Doc_CheckTemplates      1
-Doc_TemplateLibrary     $docRoot/libtml
-Doc_ErrorPage           /error.html
-Doc_NotFoundPage        /notfound.html
-Doc_Webmaster           $webmaster
-if [catch {Auth_AccessFile              .htaccess} err] {
-    Stderr "No Basic Authentication: $err"
-}
-Log_SetFile             $logDir/log${port}_
-Log_FlushMinutes        1
-
-#END CONFIGURATION
-
-# This is what I do for site-wide look and feel
-catch {
-    package require template
-    source $docRoot/.tml
+if {[catch {source $config_file} message]} then {
+    set error "Error processing configuration file \"[file nativename $config_file]\"."
+    append error "\n\t" "Error was: $message"
+    puts stderr $error
+    exit 1
 }
 
-# Start the server listening on the accept socket.
-# If you have defined ipaddr, it binds to that interface.
-# Otherwise it accepts connections on any interface.
+# Finally, start the server
 
-if {[catch {
-    if [info exists ipaddr] {
-	Httpd_Server 	$port $host $ipaddr
-    } else {
-	Httpd_Server 	$port $host
-    }
-}]} {
-    # try other ports
-    for {set port 8000} {1} {incr port} {
-        if ![catch {
-            Httpd_Server $port $host
-        }] {
-            puts stderr "Httpd on [info hostname]:$port"
-            Log_SetFile $logDir/log${port}_
-            break
-        }
-    }
-}
+Httpd_Server        $port $host $ipaddr
+
 Log_Flush
 
 # Try to increase file descriptor limits
- 
-if {[catch {
+
+if [catch {
     package require limit
     set limit 256
     limit $limit
-}]} {
-    set limit default
+} err] {
+    Stderr $err
+    set limit 64
 }
 Stderr "Running with $limit file descriptor limit"
- 
-# Try to change UID to nobody
+
+# Try to change UID to tclhttpd so we can write template caches
 
 if ![catch {
     package require setuid
-    setuid $uid
+    setuid $uid $gid
 }] {
     Stderr "Running as user $uid"
 }
 
 # Start up the user interface and event loop.
 
-if [info exists tk_version] {
+if {[info exists tk_version]} {
     SrvUI_Init "Tcl HTTPD $Httpd(version)"
 }
-catch {puts stderr "httpd started on port $port"}
-if {[lsearch -glob $argv -de*] >= 0} {
+if {$debug} {
     # Enter interactive command loop, then exit.  Otherwise run forever.
     Stdin_Start "httpd % "
     Httpd_Shutdown
 } else {
+    catch {puts stderr "httpd started on port $port"}
     vwait forever
 }
