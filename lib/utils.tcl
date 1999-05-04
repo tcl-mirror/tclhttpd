5d39ba0382aff059ac4384531d5d791e6caf102c
welch <welch>
1999-05-04 21:55:06 +0000

    Fixed lassign when there are no args.

--- lib/utils.tcl
+++ lib/utils.tcl
@@ -72,12 +72,17 @@ proc Incr {varName {value 1}} {
 # If there are fewer values than variables, the variables get the empty string.
 
 proc lassign {valueList args} {
-  if {[llength $args] == 0} {
-      error "wrong # args: lassign list varname ?varname..?"
-  }
-
-  uplevel [list foreach $args $valueList {break}]
-  return [lrange $valueList [llength $args] end]
+    if {[llength $args] == 0} {
+	error "wrong # args: lassign list varname ?varname..?"
+    }
+    if {[llength $valueList] == 0} {
+	foreach x $args {
+	    uplevel 1 [list set $x {}]
+	}
+    } else {
+	uplevel 1 [list foreach $args $valueList {break}]
+    }
+    return [lrange $valueList [llength $args] end]
 }
 # Assign a set of variables from a list of values.
 # If there are more values than variables, they are ignored.
