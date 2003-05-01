23d19ace82fc626f0d86b9105276c71f6a8ccc8e
welch <welch>
2003-05-01 23:42:11 +0000

    src/limit.c: added stubs enabled version from Michael Schlenker

--- src/limit.c
+++ src/limit.c
@@ -7,15 +7,25 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
+
+static int  LimitCmd _ANSI_ARGS_((ClientData clientData,
+            Tcl_Interp *interp, int argc, char *argv[]));
+
+#undef TCL_STORAGE_CLASS
+#define TCL_STORAGE_CLASS DLLEXPORT
+
+EXTERN int  Limit_Init _ANSI_ARGS_((Tcl_Interp *interp));
+
+
 /*
  * LimitCmd --
- *	Set resource limits.
+ *  Set resource limits.
  *
  * Results:
- *	none
+ *  none
  *
  * Side Effects:
- *	Set resource limits.
+ *  Set resource limits.
  */
 int
 LimitCmd(ClientData data, Tcl_Interp *interp, int argc, char *argv[])
@@ -25,34 +35,45 @@ LimitCmd(ClientData data, Tcl_Interp *interp, int argc, char *argv[])
     struct rlimit limit;
     Tcl_ResetResult(interp);
     if (getrlimit(RLIMIT_NOFILE, &limit) < 0) {
-	Tcl_AppendResult(interp, "NOFILE: ", Tcl_PosixError(interp), NULL);
-	return TCL_ERROR;
+    Tcl_AppendResult(interp, "NOFILE: ", Tcl_PosixError(interp), NULL);
+    return TCL_ERROR;
     }
     if (argc > 1) {
-	Tcl_GetInt(interp, argv[1], (int *)&limit.rlim_cur);
+    Tcl_GetInt(interp, argv[1], (int *)&limit.rlim_cur);
         if (setrlimit(RLIMIT_NOFILE, &limit) < 0) {
-	    Tcl_AppendResult(interp, "NOFILE: ", Tcl_PosixError(interp), NULL);
-	    return TCL_ERROR;
-	}
+        Tcl_AppendResult(interp, "NOFILE: ", Tcl_PosixError(interp), NULL);
+        return TCL_ERROR;
     }
+    }
+    /* bad, bad style, direct writing to interp->result */
     sprintf(interp->result, "%d %d", limit.rlim_cur, limit.rlim_max);
     return TCL_OK;
 }
 
 /*
  * Limit_Init --
- *	Initialize the Tcl limit facility.
+ *  Initialize the Tcl limit facility.
  *
  * Results:
- *	TCL_OK.
+ *  TCL_OK.
  *
  * Side Effects:
- *	None.
+ *  None.
  */
 int
 Limit_Init(Tcl_Interp *interp)
 {
+    int code;
+    #ifdef USE_TCL_STUBS
+        if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
+            return TCL_ERROR;
+        }
+    #endif
+
     Tcl_CreateCommand(interp, "limit", LimitCmd, NULL, NULL);
-    Tcl_PkgProvide(interp, "limit", "1.0");
+    code = Tcl_PkgProvide(interp, "limit", "1.0");
+    if (code != TCL_OK) {
+    return code;
+    }
     return TCL_OK;
 }
