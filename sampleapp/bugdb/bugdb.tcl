59e8ffe9753ba6f7787d965cbdc284725d0de29a
jeffran <jeffran>
2004-02-01 15:41:22 +0000

    Added procedure to list all bugs.

--- sampleapp/bugdb/bugdb.tcl
+++ sampleapp/bugdb/bugdb.tcl
@@ -23,3 +23,29 @@ proc bugdb::insert {Application OS Priority Assigned Summary Description} {
 
     return $result
 }
+
+proc bugdb::bug-list {} {
+
+    # Open the db
+    mk::file open bugdb ../sampleapp/bugdb/bugdb.mk
+
+    set results {}
+    set rows [mk::select bugdb.bugs]
+
+    foreach row $rows {
+        
+	# Get the contents for that row
+	lappend results $row \
+	                [mk::get bugdb.bugs!$row date] \
+	                [mk::get bugdb.bugs!$row Application] \
+	                [mk::get bugdb.bugs!$row OS] \
+	                [mk::get bugdb.bugs!$row Priority] \
+	                [mk::get bugdb.bugs!$row Summary] \
+	                [mk::get bugdb.bugs!$row Assigned]
+    }
+
+    # Close the db
+    mk::file close bugdb
+
+    return $results
+}
