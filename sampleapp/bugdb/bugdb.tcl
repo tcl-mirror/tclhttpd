fadbe79f11e7eb4fe8cddc52d95af5979468383d
jeffran <jeffran>
2004-02-01 14:10:05 +0000

    Added path to metakit database to fix insertion problem.

--- sampleapp/bugdb/bugdb.tcl
+++ sampleapp/bugdb/bugdb.tcl
@@ -5,6 +5,21 @@ namespace eval bugdb {
     namespace export *
 }
 
-proc bugdb::insert {} {
-    return Test
+proc bugdb::insert {Application OS Priority Assigned Summary Description} {
+    # set date
+    set date [clock format [clock seconds] ]
+
+    # Open the db
+    mk::file open bugdb ../sampleapp/bugdb/bugdb.mk
+
+    set result [catch {mk::row append bugdb.bugs Application "$Application" OS "$OS" \
+    Priority "$Priority" Assigned "$Assigned" Summary "$Summary" \
+    Description "$Description" date "$date"} msg]
+
+    mk::file commit bugdb
+
+    # Close the db
+    mk::file close bugdb
+
+    return $result
 }
