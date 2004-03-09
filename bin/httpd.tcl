efa4eada0e27aba36fb93062d0011475f438211d
coldstore <coldstore>
2004-03-09 07:55:54 +0000

    Changed executable tclsh8.3 to just tclsh - it's silly to specify a version explicitly like that.

--- bin/httpd.tcl
+++ bin/httpd.tcl
@@ -43,10 +43,10 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: httpd.tcl,v 1.49 2004/03/01 07:02:37 welch Exp $
+# RCS: @(#) $Id: httpd.tcl,v 1.50 2004/03/09 07:55:54 coldstore Exp $
 #
 # \
-exec tclsh8.3 "$0" ${1+"$@"}
+exec tclsh "$0" ${1+"$@"}
 
 ############
 # auto_path
